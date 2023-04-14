56173f1f492fe1951cffab2223a3bcb9750159c5
Djebcrist <djebcrist@gmail.com>
2023-04-14 08:24:04 +0200

    Commencement du Projet

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.JFrame;
+public class Main extends JFrame{
+    public Main(){
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
